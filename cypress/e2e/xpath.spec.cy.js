0f8ac1c7669763e0c3bce66c833d0ea3371102ce
vinayaktule <vinayak.nilsoft@gmail.com>
2022-12-05 23:26:17 +0530

    practice files added

--- cypress/e2e/xpath.spec.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Xpath Locators', ()=>{
-    it('find no. of products',()=>{
-        cy.visit('https://testautomationpractice.blogspot.com/')
-        
-        cy.xpath("//input[@type='text']",{timeout:10000}).should('have.length',4)
-    });
-})
